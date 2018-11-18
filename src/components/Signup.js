128d49ff38205bb8e59fe162383bcd243c0af0d2
Nguyễn Anh Khoa <ng.akhoa@yahoo.com.vn>
2018-11-18 18:02:14 +0700

    onChange function remodel

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -31,16 +31,16 @@ export default class SignUp extends React.Component{
         experience: 0
     };
 
-    this.handleChangeUsername = this.handleChangeUsername.bind(this);
-    this.handleChangePassword = this.handleChangePassword.bind(this);
-    this.handleChangeName = this.handleChangeName.bind(this);
-    this.handleChangeDescription = this.handleChangeDescription.bind(this);
-    this.handleChangeDate = this.handleChangeDate.bind(this);
-    this.handleChangeGmail = this.handleChangeGmail.bind(this);
-    this.handleChangeSex = this.handleChangeSex.bind(this);
-    this.handleChangeAddress = this.handleChangeAddress.bind(this);
-    this.handleChangePosition = this.handleChangePosition.bind(this);
-    this.handleChangeExperience = this.handleChangeExperience.bind(this);
+    // this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    // this.handleChangePassword = this.handleChangePassword.bind(this);
+    // this.handleChangeName = this.handleChangeName.bind(this);
+    // this.handleChangeDescription = this.handleChangeDescription.bind(this);
+    // this.handleChangeDate = this.handleChangeDate.bind(this);
+    // this.handleChangeGmail = this.handleChangeGmail.bind(this);
+    // this.handleChangeSex = this.handleChangeSex.bind(this);
+    // this.handleChangeAddress = this.handleChangeAddress.bind(this);
+    // this.handleChangePosition = this.handleChangePosition.bind(this);
+    // this.handleChangeExperience = this.handleChangeExperience.bind(this);
 
     this.handleChange = this.handleChange.bind(this);
   }
@@ -49,49 +49,49 @@ export default class SignUp extends React.Component{
     e.preventDefault();
     this.props.history.push('/');
   }
-  handleChange(e) {
-
+  handleChange = name => value => {
+    this.setState({[name]: value});
   }
 
-  handleChangePosition(e) {
-    this.setState({ position: e });
-  }
+  // handleChangePosition(e) {
+  //   this.setState({ position: e });
+  // }
 
-  handleChangeExperience(e) {
-    this.setState({ experience: e });
-  }
+  // handleChangeExperience(e) {
+  //   this.setState({ experience: e });
+  // }
 
-  handleChangeUsername(e){
-    this.setState({username: e});
-  }
+  // handleChangeUsername(e){
+  //   this.setState({username: e});
+  // }
 
-  handleChangePassword(e){
-    this.setState({password: e});
-  }
+  // handleChangePassword(e){
+  //   this.setState({password: e});
+  // }
 
-  handleChangeName(e){
-    this.setState({name: e});
-  }
+  // handleChangeName(e){
+  //   this.setState({name: e});
+  // }
 
-  handleChangeAddress(e){
-    this.setState({address: e});
-  }
+  // handleChangeAddress(e){
+  //   this.setState({address: e});
+  // }
 
-  handleChangeGmail(e){
-    this.setState({gmail: e});
-  }
+  // handleChangeGmail(e){
+  //   this.setState({gmail: e});
+  // }
 
-  handleChangeDescription(e){
-    this.setState({description: e});
-  }
+  // handleChangeDescription(e){
+  //   this.setState({description: e});
+  // }
 
-  handleChangeDate(e){
-    this.setState({date: e});
-  }
+  // handleChangeDate(e){
+  //   this.setState({date: e});
+  // }
 
-  handleChangeSex(e){
-    this.setState({sex: e});
-  }
+  // handleChangeSex(e){
+  //   this.setState({sex: e});
+  // }
 
 
   render() {
@@ -111,27 +111,27 @@ export default class SignUp extends React.Component{
 
             <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
               <h5 className = "col-lg-2">User name</h5>
-              <Input className = "col-lg-10" name="username" value={this.state.username} onChange={this.handleChangeUsername}></Input>
+              <Input className = "col-lg-10" name="username" value={this.state.username} onChange={this.handleChange('username')}></Input>
             </Form.Item>
 
             <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
               <h5 className = "col-lg-2">Password</h5>
-              <Input className = "col-lg-10" style={{width: 485}} type = "password" value={this.state.password} onChange={this.handleChangePassword}></Input>
+              <Input className = "col-lg-10" style={{width: 485}} type = "password" value={this.state.password} onChange={this.handleChange('password')}></Input>
             </Form.Item>
 
             <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
               <h5 className = "col-lg-2">Name</h5>
-              <Input className = "col-lg-10" value={this.state.name} onChange={this.handleChangeName}></Input>
+              <Input className = "col-lg-10" value={this.state.name} onChange={this.handleChange('name')}></Input>
             </Form.Item>
 
             <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
               <h5 className = "col-lg-2">Gmail</h5>
-              <Input className = "col-lg-10" value={this.state.gmail} onChange={this.handleChangeGmail}></Input>
+              <Input className = "col-lg-10" value={this.state.gmail} onChange={this.handleChange('gmail')}></Input>
             </Form.Item> 
 
             <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
               <h5 className = "col-lg-2">Address</h5>
-              <Input className = "col-lg-10" value={this.state.address} onChange={this.handleChangeAddress}></Input>
+              <Input className = "col-lg-10" value={this.state.address} onChange={this.handleChange('address')}></Input>
             </Form.Item>
 
                 <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
@@ -140,7 +140,7 @@ export default class SignUp extends React.Component{
                     <DatePicker
                     value={this.state.date}
                     placeholder="Pick a date"
-                    onChange={this.handleChangeDate}
+                    onChange={this.handleChange('date')}
                     />
                   </Form.Item>
                 </Form.Item>
@@ -148,25 +148,25 @@ export default class SignUp extends React.Component{
               <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
                 <h5 className = "col-lg-3">Experience</h5>
                 <div style = {{marginLeft: 140, width: "450px"}}>
-                  <Slider value={this.state.experience} onChange={this.handleChangeExperience}/>
+                  <Slider value={this.state.experience} onChange={this.handleChange('experience')}/>
                 </div>
             </Form.Item>
 
               <Form.Item style={{marginLeft: "-160px"}}>
                 <h5 className = "col-lg-2" style={{marginRight: 15}}>Sex</h5>
-                <Radio value="female" checked={this.state.sex === 'female'} onChange={this.handleChangeSex.bind(this)}>Female</Radio>
-                <Radio value="male" checked={this.state.sex === 'male'} onChange={this.handleChangeSex.bind(this)}>Male</Radio>
+                <Radio value="female" checked={this.state.sex === 'female'} onChange={this.handleChange('sex')}>Female</Radio>
+                <Radio value="male" checked={this.state.sex === 'male'} onChange={this.handleChange('sex')}>Male</Radio>
             </Form.Item> 
 
             <Form.Item style={{marginLeft: "-160px"}}>
               <h5 className = "col-lg-2" style={{marginRight: 15}}>Position</h5>
-              <Radio value="helper" checked={this.state.positon === 'helper'} onChange={this.handleChangePosition.bind(this)}>Helper</Radio>
-              <Radio value="hirer" checked={this.state.position === 'hirer'} onChange={this.handleChangePosition.bind(this)}>Hire</Radio>
+              <Radio value="helper" checked={this.state.positon === 'helper'} onChange={this.handleChange('position')}>Helper</Radio>
+              <Radio value="hirer" checked={this.state.position === 'hirer'} onChange={this.handleChange('position')}>Hire</Radio>
             </Form.Item> 
 
              <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
               <h5 className = "col-lg-2">Description</h5>
-              <Input className = "col-lg-10" type="textarea" style={{width: 500, marginTop: 40}} value={this.state.description} onChange={this.handleChangeDescription}></Input>
+              <Input className = "col-lg-10" type="textarea" style={{width: 500, marginTop: 40}} value={this.state.description} onChange={this.handleChange('description')}></Input>
               </Form.Item>
 
             <Form.Item style={{marginTop: 30}}>
