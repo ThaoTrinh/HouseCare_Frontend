7cb9c9df337b97678110efba4b573d14ae1a488a
thaotrinh <trinhthithuthaopy@gmail.com>
2018-11-18 15:55:18 +0700

    dont render

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,74 +1,71 @@
 import React from 'react';
-import { Button , Layout, Form, Input, Checkbox, Select, Radio, Switch, DatePicker } from 'element-react';
+import {Link} from 'react-router';
+import { Button , Layout, Form, Input, Checkbox, Select, Radio, Switch, DatePicker, Slider } from 'element-react';
 import 'element-theme-default';
 import MainSlider from "./MainSlider"
 
 export default class SignIn extends React.Component{
-    constructor(props) {
-        super(props);
-      
-        this.state = {
-          form: {
-            name: '',
-            sex: '',
-            date1: null,
-            date2: null,
-            public: false,
-            type: [],
-            position: '',
-            desc: '',
-            address: '',
-            username: '',
-            password: ''
-          }
-        };
-      }
-      
-      onSubmit(e) {
-        e.preventDefault();
-      }
-      
-      onChange(key, value) {
-        this.state.form[key] = value;
-        this.forceUpdate();
-      }
-      
-      render() {
-        return (
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+        username: '',
+        password: ''
+    };
 
-        <Layout.Row>
-            <Layout.Col span="12">
-                <div className="grid-content bg-purple">
-                    <img src="/logo/logup.jpg" style={{height: 500, width: 500}}/>
-                </div>
-            </Layout.Col>
+    this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+  }
+
+  onSubmit(e) {
+
+    e.preventDefault();
+    
+  }
+  handleChangeUsername(e){
+    this.setState({username: e});
+  }
+
+  handleChangePassword(e){
+    this.setState({password: e});
+  }
 
 
-            <Layout.Col span="12">
-                <div className="grid-content bg-purple-light">
-                    <Form className="en-US" model={this.state.form} labelWidth="120" onSubmit={this.onSubmit.bind(this)}>
+  render() {
+    return (
 
-                        <Form.Item style={{marginLeft: "-160px", marginTop: 200}}>
-                            <h5 className = "col-lg-2">User name</h5>
-                            <Input className = "col-lg-10" value={this.state.form.username} onChange={this.onChange.bind(this, 'username')}></Input>
-                        </Form.Item>
-
-                        <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
-                            <h5 className = "col-lg-2">Password</h5>
-                            <Input className = "col-lg-10" style={{width: 485}} type = "password" value={this.state.form.password} onChange={this.onChange.bind(this, 'password')}></Input>
-                        </Form.Item>
-
-                         <Form.Item style={{marginTop: 30}}>
-                            <Button type="primary" nativeType="submit" style={{marginLeft: 60}}>Sign in</Button>
-                            <Button style={{marginLeft: 140}}>Cancel</Button>
-                        </Form.Item>
-                       
-                        </Form>
-                </div>
-            </Layout.Col>
-          </Layout.Row>
+    <Layout.Row>
+      <Layout.Col span="12">
+        <div className="grid-content bg-purple">
+          <img src="/logo/logup.jpg" style={{height: 640, width: 620}}/>
+        </div>
+      </Layout.Col>
 
 
-        )
-      }
+      <Layout.Col span="12">
+        <div className="grid-content bg-purple-light" style={{marginTop: 200}}>
+          <Form className="en-US" model={this.state} labelWidth="120" onSubmit={this.onSubmit.bind(this)}>
+
+            <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
+              <h5 className = "col-lg-2">User name</h5>
+              <Input className = "col-lg-10" value={this.state.username} onChange={this.handleChangeUsername}></Input>
+            </Form.Item>
+
+            <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
+              <h5 className = "col-lg-2">Password</h5>
+              <Input className = "col-lg-10" style={{width: 485}} type = "password" value={this.state.password} onChange={this.handleChangePassword}></Input>
+            </Form.Item>
+
+
+            <Form.Item style={{marginTop: 30}}>
+              <Link to='/'><Button type="primary" nativeType="submit" style={{marginLeft: 60}}>Sign In</Button></Link>
+              <Button style={{marginLeft: 140}}>Cancel</Button>
+            </Form.Item>
+
+          </Form>
+        </div>
+      </Layout.Col>
+    </Layout.Row>
+    )
+  }
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,130 +1,167 @@
 import React from 'react';
-import { Button , Layout, Form, Input, Checkbox, Select, Radio, Switch, DatePicker } from 'element-react';
+import { Button , Layout, Form, Input, Checkbox, Select, Radio, Switch, DatePicker, Slider } from 'element-react';
 import 'element-theme-default';
 import MainSlider from "./MainSlider"
-
+import createHistory from 'history/createBrowserHistory'
 export default class SignUp extends React.Component{
-    constructor(props) {
-        super(props);
-      
-        this.state = {
-          form: {
-            name: '',
-            sex: '',
-            date1: null,
-            date2: null,
-            public: false,
-            type: [],
-            position: '',
-            desc: '',
-            address: '',
-            username: '',
-            password: ''
-          }
-        };
-      }
-      
-      onSubmit(e) {
-        e.preventDefault();
-      }
-      
-      onChange(key, value) {
-        this.state.form[key] = value;
-        this.forceUpdate();
-      }
-      
-      render() {
-        return (
+  constructor(props) {
+    super(props);
+   
+    this.state = {
+        username: '',
+        password: '',
+        name: '',
+        gmail: '',
+        address:'',
+        date:'',
+        sex: 'female',
+        description: '',
+        position: 'helper',
+        experience: 0
+    };
 
-        <Layout.Row>
-            <Layout.Col span="12">
-                <div className="grid-content bg-purple">
-                    <img src="/logo/logup.jpg" style={{height: 640, width: 620}}/>
+    this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeDescription = this.handleChangeDescription.bind(this);
+    this.handleChangeDate = this.handleChangeDate.bind(this);
+    this.handleChangeGmail = this.handleChangeGmail.bind(this);
+    this.handleChangeSex = this.handleChangeSex.bind(this);
+    this.handleChangeAddress = this.handleChangeAddress.bind(this);
+    this.handleChangePosition = this.handleChangePosition.bind(this);
+    this.handleChangeExperience = this.handleChangeExperience.bind(this);
+  }
+
+  onSubmit(e) { const history = createHistory()
+    e.preventDefault();
+    console.log("aaaa")
+    history.push({ pathname: '/' })
+  }
+  handleChangePosition(e) {
+    this.setState({ position: e });
+  }
+
+  handleChangeExperience(e) {
+    this.setState({ experience: e });
+  }
+
+  handleChangeUsername(e){
+    this.setState({username: e});
+  }
+
+  handleChangePassword(e){
+    this.setState({password: e});
+  }
+
+  handleChangeName(e){
+    this.setState({name: e});
+  }
+
+  handleChangeAddress(e){
+    this.setState({address: e});
+  }
+
+  handleChangeGmail(e){
+    this.setState({gmail: e});
+  }
+
+  handleChangeDescription(e){
+    this.setState({description: e});
+  }
+
+  handleChangeDate(e){
+    this.setState({date: e});
+  }
+
+  handleChangeSex(e){
+    this.setState({sex: e});
+  }
+
+
+  render() {
+    return (
+
+    <Layout.Row>
+      <Layout.Col span="12">
+        <div className="grid-content bg-purple">
+          <img src="/logo/logup.jpg" style={{height: 900, width: 620}}/>
+        </div>
+      </Layout.Col>
+
+
+      <Layout.Col span="12">
+        <div className="grid-content bg-purple-light">
+          <Form className="en-US" model={this.state} labelWidth="120" onSubmit={this.onSubmit.bind(this)}>
+
+            <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
+              <h5 className = "col-lg-2">User name</h5>
+              <Input className = "col-lg-10" value={this.state.username} onChange={this.handleChangeUsername}></Input>
+            </Form.Item>
+
+            <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
+              <h5 className = "col-lg-2">Password</h5>
+              <Input className = "col-lg-10" style={{width: 485}} type = "password" value={this.state.password} onChange={this.handleChangePassword}></Input>
+            </Form.Item>
+
+            <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
+              <h5 className = "col-lg-2">Name</h5>
+              <Input className = "col-lg-10" value={this.state.name} onChange={this.handleChangeName}></Input>
+            </Form.Item>
+
+            <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
+              <h5 className = "col-lg-2">Gmail</h5>
+              <Input className = "col-lg-10" value={this.state.gmail} onChange={this.handleChangeGmail}></Input>
+            </Form.Item> 
+
+            <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
+              <h5 className = "col-lg-2">Address</h5>
+              <Input className = "col-lg-10" value={this.state.address} onChange={this.handleChangeAddress}></Input>
+            </Form.Item>
+
+                <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
+                  <h5 className = "col-lg-4" style={{marginRight: "-118px"}}>Date of birth</h5>
+                  <Form.Item prop="date1" labelWidth="0px" className = "col-lg-8">
+                    <DatePicker
+                    value={this.state.date}
+                    placeholder="Pick a date"
+                    onChange={this.handleChangeDate}
+                    />
+                  </Form.Item>
+                </Form.Item>
+
+              <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
+                <h5 className = "col-lg-3">Experience</h5>
+                <div style = {{marginLeft: 140, width: "450px"}}>
+                  <Slider value={this.state.experience} onChange={this.handleChangeExperience}/>
                 </div>
-            </Layout.Col>
+            </Form.Item>
 
+              <Form.Item style={{marginLeft: "-160px"}}>
+                <h5 className = "col-lg-2" style={{marginRight: 15}}>Sex</h5>
+                <Radio value="female" checked={this.state.sex === 'female'} onChange={this.handleChangeSex.bind(this)}>Female</Radio>
+                <Radio value="male" checked={this.state.sex === 'male'} onChange={this.handleChangeSex.bind(this)}>Male</Radio>
+            </Form.Item> 
 
-            <Layout.Col span="12">
-                <div className="grid-content bg-purple-light">
-                    <Form className="en-US" model={this.state.form} labelWidth="120" onSubmit={this.onSubmit.bind(this)}>
+            <Form.Item style={{marginLeft: "-160px"}}>
+              <h5 className = "col-lg-2" style={{marginRight: 15}}>Position</h5>
+              <Radio value="helper" checked={this.state.positon === 'helper'} onChange={this.handleChangePosition.bind(this)}>Helper</Radio>
+              <Radio value="hirer" checked={this.state.position === 'hirer'} onChange={this.handleChangePosition.bind(this)}>Hire</Radio>
+            </Form.Item> 
 
-                        <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
-                            <h5 className = "col-lg-2">User name</h5>
-                            <Input className = "col-lg-10" value={this.state.form.username} onChange={this.onChange.bind(this, 'username')}></Input>
-                        </Form.Item>
+             <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
+              <h5 className = "col-lg-2">Description</h5>
+              <Input className = "col-lg-10" type="textarea" style={{width: 500, marginTop: 40}} value={this.state.description} onChange={this.handleChangeDescription}></Input>
+              </Form.Item>
 
-                        <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
-                            <h5 className = "col-lg-2">Password</h5>
-                            <Input className = "col-lg-10" style={{width: 485}} type = "password" value={this.state.form.password} onChange={this.onChange.bind(this, 'password')}></Input>
-                        </Form.Item>
+            <Form.Item style={{marginTop: 30}}>
+              <Button type="primary" nativeType="submit" style={{marginLeft: 60}}>Sign up</Button>
+              <Button style={{marginLeft: 140}}>Cancel</Button>
+            </Form.Item>
 
-                        <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
-                            <h5 className = "col-lg-2">Name</h5>
-                            <Input className = "col-lg-10" value={this.state.form.name} onChange={this.onChange.bind(this, 'name')}></Input>
-                        </Form.Item>
-
-                        <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
-                            <h5 className = "col-lg-2">Address</h5>
-                            <Input className = "col-lg-10" value={this.state.form.address} onChange={this.onChange.bind(this, 'address')}></Input>
-                        </Form.Item>
-
-                        <div className = "col-lg-12" style={{left: "-33px"}}>
-                        <div className = "col-lg-7">
-                        <Form.Item style={{marginLeft: "-158px"}}>
-                            <h5 className = "col-lg-4" style={{marginRight: "-5px"}}>Date of birth</h5>
-                            <Form.Item prop="date1" labelWidth="0px" className = "col-lg-8">
-                                <DatePicker
-                                value={this.state.form.date1}
-                                placeholder="Pick a date"
-                                onChange={this.onChange.bind(this, 'date1')}
-                                />
-                            </Form.Item>
-                        </Form.Item>
-                        </div>
-                        <div className = "col-lg-5">
-                        <Form.Item style={{marginLeft: "-190px", marginRight: 35}}>
-                            <h5 className = "col-lg-2">Sex</h5>
-                            <Select value={this.state.form.sex} placeholder="Select your sex" className = "col-lg-10">
-                            <Select.Option label="Male" value="shanghai"></Select.Option>
-                            <Select.Option label="Female" value="beijing"></Select.Option>
-                            </Select>
-                        </Form.Item>
-                        </div>
-                        </div>
-
-                        <Form.Item style={{marginLeft: "-160px", marginTop: 30}}>
-                            <h5 className = "col-lg-3">Year of experience</h5>
-                            <Checkbox.Group value={this.state.form.type} onChange={this.onChange.bind(this, 'type')}>
-                            <Checkbox label="0 - 1" name="type" style={{marginRight: 20}}></Checkbox>
-                            <Checkbox label="1 - 3" name="type" style={{marginRight: 20}}></Checkbox>
-                            <Checkbox label="3 - 5" name="type" style={{marginRight: 20}}></Checkbox>
-                            <Checkbox label="Over 5" name="type" style={{marginRight: 20}}></Checkbox>
-                            </Checkbox.Group>
-                        </Form.Item> 
-
-                       <Form.Item style={{marginLeft: "-160px"}}>
-                            <h5 className = "col-lg-2" style={{marginRight: 60}}>Position</h5>
-                            <Radio.Group value={this.state.form.position}>
-                            <Radio value="Hirer" style={{marginRight: 15}}></Radio>
-                            <Radio value="House helper"></Radio>
-                            </Radio.Group>
-                        </Form.Item>
-
-                        <Form.Item style={{marginLeft: "-160px"}}>
-                            <h5 className = "col-lg-2">Description</h5>
-                            <Input className = "col-lg-10" type="textarea" style={{width: 500}} value={this.state.form.desc} onChange={this.onChange.bind(this, 'desc')}></Input>
-                        </Form.Item>
-
-                        <Form.Item style={{marginTop: 30}}>
-                            <Button type="primary" nativeType="submit" style={{marginLeft: 60}}>Sign up</Button>
-                            <Button style={{marginLeft: 140}}>Cancel</Button>
-                        </Form.Item>
-                        </Form>
-                </div>
-            </Layout.Col>
-          </Layout.Row>
-
-
-        )
-      }
+          </Form>
+        </div>
+      </Layout.Col>
+    </Layout.Row>
+    )
+  }
 }
